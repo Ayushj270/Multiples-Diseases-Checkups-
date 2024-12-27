b8ddb4bd4fd580b5ae9c3a3fbead231e93d5836b
JAyush18 <161866822+Ayushj270@users.noreply.github.com>
2024-12-27 17:12:54 +0000

    updated full code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+
 #! Import Libraries
 import pickle
 import streamlit as st
 
 #! Load the saved models
 diabetes = pickle.load(open('diabetes.sav', 'rb'))
-heart = pickle.load(open('git heart_data.sav', 'rb'))
+heart = pickle.load(open('heart_data.sav', 'rb'))
 parkinsons = pickle.load(open('parkinsons_model.sav', 'rb'))
 
-#! Initializze session state
+#! Initialize session state
 if 'page' not in st.session_state:
     st.session_state.page = 'main_page'
 
@@ -16,14 +17,17 @@ if st.session_state.page == 'main_page':
     st.title('Disease Prediction System')
     st.subheader('Select the type of disease to predict:')
     disease = st.selectbox('Choose a prediction system:', 
-                           ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'])    
-    if st.button('Next'):
+                           ['Select Your Prediction Mechanism','Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'])  
+
+    if disease != 'Select Your Prediction Mechanism' and st.button('Next'):
         st.session_state.disease = disease
-        st.session_state.page = 'Prediction_page'
+        st.session_state.page = 'prediction_page'
 
 #! Diabetes Prediction
 elif st.session_state.page == 'prediction_page':
     disease = st.session_state.disease
+
+    #! Diabetes prediction
     if disease == 'Diabetes Prediction':
         st.title('Diabetes Prediction')
         st.subheader("Check Gender.")
@@ -31,148 +35,173 @@ elif st.session_state.page == 'prediction_page':
         #! Gender Selection
         def gender_selection():
             gender = st.selectbox('Gender', ['None', 'Male', 'Female'])
-            if gender == 'None':
+            if gender != 'None':
+                st.write(f'You are a {gender}.')
+                return gender
+            else:
                 st.write('Please select a gender.')
-            elif gender == 'Male':
-                st.write('You are a Male')
+                return None
+
+        #! Input Fields
+        def input_fields(gender):
+            col1, col2 = st.columns(2)
+            if gender == 'Male':
+                with col1:
+                    pregnancies = 0
+                    glucose = st.number_input('Glucose Level', min_value=0.0, max_value=200.0, step=0.1)
+                    bp = st.number_input('Blood Pressure value', min_value=0.0, max_value=122.0, step=0.1)
+                    skinthickness = st.number_input('Skin Thickness value', min_value=0.0, max_value=99.0, step=0.1)
+                    insulin = st.number_input('Insulin Level', min_value=0.0, max_value=846.0, step=0.1)
+                with col2:
+                    bmi = st.number_input('BMI value', min_value=0.0, max_value=67.1, step=0.1)
+                    dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.42, step=0.1)
+                    age = st.number_input('Age', min_value=0, max_value=81, step=1)
+           
             elif gender == 'Female':
-                st.write('You are a Female')
-            return gender
+                with col1:
+                    pregnancies = st.number_input('Number of Pregnancies', min_value=0, max_value=17, step=1)
+                    glucose = st.number_input('Glucose Level', min_value=0.0, max_value=200.0, step=0.1)
+                    bp = st.number_input('Blood Pressure value', min_value=0.0, max_value=122.0, step=0.1)
+                    skinthickness = st.number_input('Skin Thickness value', min_value=0.0, max_value=99.0, step=0.1)
+                with col2:
+                    insulin = st.number_input('Insulin Level', min_value=0.0, max_value=846.0, step=0.1)
+                    bmi = st.number_input('BMI value', min_value=0.0, max_value=67.1, step=0.1)
+                    dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.42, step=0.1)
+                    age = st.number_input('Age', min_value=0, max_value=81, step=1)
+            else:
+                st.error("Invaild Gender Selection")
+                return None, None, None, None, None, None, None, None
+
+            return pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age
+
+        #! Prediction
+        def prediction(pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age):
+            if st.button('Predict Diabetes'):
+                if age == 0:
+                    st.error("Please enter valid input")
+
+                else:
+                    try:
+                        prediction = diabetes.predict([[pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age]])
+                        result = 'Diabetic' if prediction[0] == 1 else 'Not Diabetic'
+                        st.success(f'Result: The person is {result}')
+                    except:
+                        st.error("Please enter valid input")
+
+            if st.button('Go Back', key='back_to_main_from_diabetes'):
+                st.session_state.page = 'main_page'
 
         gender = gender_selection()
-        if gender is not None and st.button('Next'):
-            st.session_state.gender = gender
-            st.session_state.page = 'input_page'
+        if gender:
+            if st.button('Next', key='to_input_page'):
+                st.session_state.page = 'input_page'
 
-            #! Input Fields
-            def inputs_fields(gender):
-                col1, col2 = st.columns(2)
-                if gender == 'Male':
-                    with col1:
-                        pregnancies = 0
-                        glucose = st.number_input('Glucose Level',min = 0, max = 200, step = 1, disabled = gender == 'None')
-                        bp = st.number_input('Blood Pressure value', min = 0, max = 122, step = 0.1, disabled = gender == 'None')
-                        skinthickness = st.number_input('Skin Thickness value', min=0, max = 99, step = 0.1,disabled = gender == 'None')
-                        insulin = st.number_input('Insulin Level', min = 0, max = 846,  step = 0.1, disabled = gender == 'None')
-                    with col2:
-                        bmi = st.number_input('BMI value', min = 0, max = 67.1,step = 0.1, disabled = gender == 'None')
-                        dpf = st.number_input('Diabetes Pedigree Function', min = 0, max = 2.42,step = 0.1, disabled = gender == 'None')
-                        age = st.number_input('Age', min = 21, max = 81,step = 1, disabled = gender == 'None')
+        st.subheader("Provide the required details for diabetes prediction.")
+        pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age = input_fields(gender)
 
-                elif gender == 'Female':
-                    with col1:
-                        pregnancies = st.number_input('Number of Pregnancies', min = 0, max = 17, step = 1,disabled = gender == 'None')
-                        glucose = st.number_input('Glucose Level',min = 0, max = 200, step = 1, disabled = gender == 'None')
-                        bp = st.number_input('Blood Pressure value', min = 0, max = 122, step = 0.1, disabled = gender == 'None')
-                        skinthickness = st.number_input('Skin Thickness value', min=0, max = 99, step = 0.1,disabled = gender == 'None')
-                        insulin = st.number_input('Insulin Level', min = 0, max = 846,  step = 0.1, disabled = gender == 'None')
-                    with col2:
-                        bmi = st.number_input('BMI value', min = 0, max = 67.1,step = 0.1, disabled = gender == 'None')
-                        dpf = st.number_input('Diabetes Pedigree Function', min = 0, max = 2.42,step = 0.1, disabled = gender == 'None')
-                        age = st.number_input('Age', min = 21, max = 81,step = 1, disabled = gender == 'None')
-                return pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age
-
-                st.subheader("Provide the required details for diabetes prediction.")
-                pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age = inputs_fields(gender)
-
-            #! Prediction
-            prediction_disabled = (gender is None)
-            if st.button('Predict Diabetes',disabled=prediction_disabled):
-                try:
-                    prediction = diabetes.predict([[pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age]])
-                    result = 'Diabetic' if prediction[0] == 1 else 'Not Diabetic'
-                    st.success(f'Result: The person is {result}')
-                except:
-                    st.error("Please enter valid input")
-        if st.button('Go Back'):
-            st.session_state.page = 'Diabetes Prediction'
+        if pregnancies is not None:
+            prediction(pregnancies, glucose, bp, skinthickness, insulin, bmi, dpf, age)
 
     #! Heart Disease Prediction
     elif disease == 'Heart Disease Prediction':
         st.title('Heart Disease Prediction')
-        st.write("Provide the required details for heart disease prediction.")
+        st.subheader("Provide the required details for heart disease prediction.")
         
         #! Input fields
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            age = st.number_input('Age')
-            sex = st.number_input('Sex')
-        with col2:
-            cp = st.number_input('Chest Pain types')
-            trestbps = st.number_input('Resting Blood Pressure')
-        with col3:
-            chol = st.number_input('Serum Cholestoral in mg/dl')        
-            fbs = st.number_input('Fasting Blood Sugar > 120 mg/dl')
-        with col1:
-            restecg = st.number_input('Resting Electrocardiographic results')
-            thalach = st.number_input('Maximum Heart Rate achieved')
-        with col2:
-            exang = st.number_input('Exercise Induced Angina')
-            oldpeak = st.number_input('ST depression induced by exercise')
-        with col3:
-            slope = st.number_input('Slope of the peak exercise ST segment')
-            ca = st.number_input('Major vessels colored by flourosopy')
-        with col1:
-            thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        def input_fields():
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                age = st.number_input('Age', min_value=0, max_value=100, step=1)
+                sex = st.number_input('Sex', min_value=0, max_value=1, step=1)
+                cp = st.number_input('Chest Pain types', min_value=0, max_value=3, step=1)
+                trestbps = st.number_input('Resting Blood Pressure', min_value=0, max_value=200, step=1)
+                chol = st.number_input('Serum Cholestoral in mg/dl', min_value=0, max_value=600, step=1)
+            with col2:
+                fbs = st.number_input('Fasting Blood Sugar > 120 mg/dl', min_value=0, max_value=1, step=1)
+                restecg = st.number_input('Resting Electrocardiographic results', min_value=0, max_value=2, step=1)
+                thalach = st.number_input('Maximum Heart Rate achieved', min_value=0, max_value=200, step=1)
+                exang = st.number_input('Exercise Induced Angina', min_value=0, max_value=1, step=1)
+            with col3:
+                oldpeak = st.number_input('ST depression induced by exercise', min_value=0.0, max_value=6.2, step=0.1)
+                slope = st.number_input('Slope of the peak exercise ST segment', min_value=0, max_value=2, step=1)
+                ca = st.number_input('Major vessels colored by flourosopy', min_value=0, max_value=3, step=1)
+                thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect', min_value=0, max_value=2, step=1)
+
+            return age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal
 
         #! Prediction
-        if st.button('Predict Heart Disease'):
-            try:
-                prediction = heart.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
-                result = 'Heart Disease' if prediction[0] == 1 else 'Healthy'
-                st.success(f'Result: The person is {result}')
-            except:
-                st.error("Please enter valid input")
-        if st.button('Go Back'):
-            st.session_state.page = 'main_page'
+        def prediction(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal):
+            if st.button('Predict Heart Disease'):
+                if age == 0:
+                    st.error("Please enter valid input")
+
+                else:
+                    try:
+                        prediction = diabetes.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+                        result = 'Heart Disease' if prediction[0] == 1 else 'Healthy'
+                        st.success(f'Result: The person is {result}')
+                    except:
+                        st.error("Please enter valid input")
+            if st.button('Go Back'):
+                st.session_state.page = 'main_page'
+        
+        age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal = input_fields()
+
+        if age != 0:
+            prediction(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
 
     #! Parkinsons Prediction
     elif disease == 'Parkinsons Prediction':
         st.title('Parkinsons Prediction')
-        st.write("Provide the required details for Parkinsons prediction.")
+        st.subheader("Provide the required details for Parkinsons prediction.")
         
         #! Input fields
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            fo = st.number_input('MDVP:Fo(Hz)')
-            fhi = st.number_input('MDVP:Fhi(Hz)')
-        with col2:
-            flo = st.number_input('MDVP:Flo(Hz)')
-            Jitter_percent = st.number_input('MDVP:Jitter(%)')
-            Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-        with col3:
-            RAP = st.number_input('MDVP:RAP')
-            PPQ = st.number_input('MDVP:PPQ')
-        with col4:
-            DDP = st.number_input('Jitter:DDP')
-            Shimmer = st.number_input('MDVP:Shimmer')
-        with col1:
-            Shimmer_dB = st.number_input('MDVP:Shimmer(dB)')
-            APQ3 = st.number_input('Shimmer:APQ3')
-            APQ5 = st.number_input('Shimmer:APQ5')
-        with col2:
-            APQ = st.number_input('MDVP:APQ')
-            DDA = st.number_input('MDVP:DDA')
-        with col3:
-            NHR = st.number_input('NHR')
-            HNR = st.number_input('HNR')
-        with col4:
-            RPDE = st.number_input('RPDE')
-            DFA = st.number_input('DFA')
-        with col1:
-            spread1 = st.number_input('spread1')    
-            spread2 = st.number_input('spread2')
-        with col2:
-            D2 = st.number_input('D2')
-            PPE = st.number_input('PPE')
+        def input_fields():
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                fo = st.number_input('MDVP:Fo(Hz)', min_value=0.0, max_value=200.0, step=0.1)
+                fhi = st.number_input('MDVP:Fhi(Hz)', min_value=0.0, max_value=200.0, step=0.1)
+                flo = st.number_input('MDVP:Flo(Hz)', min_value=0.0, max_value=200.0, step=0.1)
+                Jitter_percent = st.number_input('MDVP:Jitter(%)', min_value=0.0, max_value=1.0, step=0.1)
+                Jitter_Abs = st.number_input('MDVP:Jitter(Abs)', min_value=0.0, max_value=1.0, step=0.1)
+                RAP = st.number_input('MDVP:RAP', min_value=0.0, max_value=1.0, step=0.1)
+                PPQ = st.number_input('MDVP:PPQ', min_value=0.0, max_value=1.0, step=0.1)
+            with col2:
+                DDP = st.number_input('Jitter:DDP', min_value=0.0, max_value=1.0, step=0.1)
+                Shimmer = st.number_input('MDVP:Shimmer', min_value=0.0, max_value=1.0, step=0.1)
+                Shimmer_dB = st.number_input('MDVP:Shimmer(dB)', min_value=0.0, max_value=1.0, step=0.1)
+                APQ3 = st.number_input('Shimmer:APQ3', min_value=0.0, max_value=1.0, step=0.1)
+                APQ5 = st.number_input('Shimmer:APQ5', min_value=0.0, max_value=1.0, step=0.1)
+                APQ = st.number_input('MDVP:APQ', min_value=0.0, max_value=1.0, step=0.1)
+                DDA = st.number_input('MDVP:DDA', min_value=0.0, max_value=1.0, step=0.1)
+            with col3:
+                NHR = st.number_input('NHR', min_value=0.0, max_value=1.0, step=0.1)
+                HNR = st.number_input('HNR', min_value=0.0, max_value=1.0, step=0.1)
+                RPDE = st.number_input('RPDE', min_value=0.0, max_value=1.0, step=0.1)
+                DFA = st.number_input('DFA', min_value=0.0, max_value=1.0, step=0.1)
+                spread1 = st.number_input('spread1', min_value=0.0, max_value=1.0, step=0.1)
+                spread2 = st.number_input('spread2', min_value=0.0, max_value=1.0, step=0.1)
+                D2 = st.number_input('D2', min_value=0.0, max_value=1.0, step=0.1)
+            PPE = st.number_input('PPE', min_value=0.0, max_value=1.0, step=0.1)
+
+            return fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE
 
         #! Prediction
-        if st.button('Predict Parkinsons'):
-            try:
-                prediction = parkinsons.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]])
-                result = 'Parkinsons' if prediction[0] == 1 else 'Healthy'
-                st.success(f'Result: The person is {result}')
-            except:
-                st.error("Please enter valid input")
-        if st.button('Go Back'):
-            st.session_state.page = 'main_page'
+        def prediction(fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE):
+            if st.button('Result'):
+                if fo == 0:
+                    st.error("Please enter valid input")
+                else:
+                    try:
+                        prediction = diabetes.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+                        result = 'Heart Disease' if prediction[0] == 1 else 'Healthy'
+                        st.success(f'Result: The person is {result}')
+                    except:
+                        st.error("Please enter valid input")
+            if st.button('Go Back'):
+                st.session_state.page = 'main_page'
+        
+        fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE = input_fields()
+
+        if fo != 0:
+            prediction(fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE)
